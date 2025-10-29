b4b916878c1b9cc455406cf722daa2ba971f5ccf
Flora-algo <ffu405585@gmail.com>
2025-10-29 15:03:26 +0800

    Initial Commit

---
+++ life countdown/life_countdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  life_countdownApp.swift
+//  life countdown
+//
+//  Created by Flora on 2025/10/29.
+//
+
+import SwiftUI
+
+@main
+struct life_countdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ life countdownTests/life_countdownTests.swift
@@ -0,0 +1,17 @@
+//
+//  life_countdownTests.swift
+//  life countdownTests
+//
+//  Created by Flora on 2025/10/29.
+//
+
+import Testing
+@testable import life_countdown
+
+struct life_countdownTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
